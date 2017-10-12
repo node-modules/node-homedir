bfb483c381e89f1f0f8f02a85a53555a6bd1b02b
popomore <sakura9515@gmail.com>
2017-10-12 10:24:38 +0800

    feat: add MOCK_HOME_DIR for test

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const os = require('os');
 
 module.exports = () => {
+  if (process.env.MOCK_HOME_DIR) return process.env.MOCK_HOME_DIR;
   if (os.userInfo && os.userInfo().homedir) {
     return os.userInfo().homedir;
   } else if (os.homedir) {
